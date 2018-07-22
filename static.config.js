e112f07de55ff959e9e29f76f21bd222ed34dae2
Eric Chaves <erchaves@gmail.com>
2018-07-22 16:15:43 -0400

    cleanup up this quasi plugin

--- static.config.js
+++ static.config.js
@@ -1,45 +1,52 @@
 import axios from 'axios'
-import ExtractCssChunks from 'extract-css-chunks-webpack-plugin'
+
+// This is taken from
+// https://github.com/nozzle/react-static/blob/master/examples/sass/plugins/react-static-plugin-sass.js
+// but i cant figure out how it's supposed to work as a proper react-static plugin
+const reactStaticPluginSass = (config, { stage }) => {
+  const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+  let loaders = []
+  let includePaths = []
+
+  const sassLoaderPath = require.resolve('sass-loader')
+
+  const sassLoader = {
+    loader: sassLoaderPath,
+    options: { includePaths: ['src/', ...includePaths] },
+  }
+  const styleLoader = { loader: 'style-loader' }
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      importLoaders: 1,
+      minimize: stage === 'prod',
+      sourceMap: false,
+    },
+  }
+
+  if (stage === 'dev') {
+    // Dev
+    loaders = [styleLoader, cssLoader, sassLoader]
+  } else if (stage === 'node') {
+    // Node
+    // Don't extract css to file during node build process
+    loaders = [cssLoader, sassLoader]
+  } else {
+    // Prod
+    loaders = [ExtractCssChunks.loader, cssLoader, sassLoader]
+  }
+
+  config.module.rules[0].oneOf.unshift({
+    test: /\.s(a|c)ss$/,
+    use: loaders,
+  })
+
+  return config
+};
 
 export default {
   webpack: (config, { stage }) => {
-    let loaders = []
-    let includePaths = []
-
-    const sassLoaderPath = require.resolve('sass-loader')
-
-    const sassLoader = {
-      loader: sassLoaderPath,
-      options: { includePaths: ['src/', ...includePaths] },
-    }
-    const styleLoader = { loader: 'style-loader' }
-    const cssLoader = {
-      loader: 'css-loader',
-      options: {
-        importLoaders: 1,
-        minimize: stage === 'prod',
-        sourceMap: false,
-      },
-    }
-
-    if (stage === 'dev') {
-      // Dev
-      loaders = [styleLoader, cssLoader, sassLoader]
-    } else if (stage === 'node') {
-      // Node
-      // Don't extract css to file during node build process
-      loaders = [cssLoader, sassLoader]
-    } else {
-      // Prod
-      loaders = [ExtractCssChunks.loader, cssLoader, sassLoader]
-    }
-
-    config.module.rules[0].oneOf.unshift({
-      test: /\.s(a|c)ss$/,
-      use: loaders,
-    })
-
-    return config
+    return reactStaticPluginSass(config, { stage });
   },
   getSiteData: () => ({
     title: 'React Static',
