e45c27a1480005677be9feb06514e14363f4ecf7
Eric Chaves <erchaves@gmail.com>
2018-07-22 16:23:04 -0400

    fixing old demo

--- static.config.js
+++ static.config.js
@@ -52,9 +52,33 @@ export default {
     title: 'React Static',
   }),
   getRoutes: async () => {
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
     return [
       {
-        path: '/blog'
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        path: '404',
+        component: 'src/containers/404',
       },
     ]
   },
